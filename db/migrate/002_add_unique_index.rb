b7671c91c915e5d2ab3d85aab2898372b8391f9a
Vincent Robert <vincent.robert@nanego.com>
2014-10-29 16:52:43 +0100

    Do not create index if exists

--- db/migrate/002_add_unique_index.rb
+++ db/migrate/002_add_unique_index.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndex < ActiveRecord::Migration
   def change
-    add_index :unregistered_watchers, [:issue_id, :email], :unique => true
+    add_index :unregistered_watchers, [:issue_id, :email], :unique => true unless index_exists?(:unregistered_watchers, [:issue_id, :email], :unique => true)
   end
 end
