6933c509ddaa3ce5098df6859add7123f81513a0
Vincent Robert <vincent.robert@nanego.com>
2014-10-29 16:56:45 +0100

    Do not create index if exists

--- db/migrate/003_create_unregistered_watchers_notifications.rb
+++ db/migrate/003_create_unregistered_watchers_notifications.rb
@@ -5,8 +5,8 @@ class CreateUnregisteredWatchersNotifications < ActiveRecord::Migration
       t.references :project
       t.text :email_body
     end unless ActiveRecord::Base.connection.table_exists? 'unregistered_watchers_notifications'
-    add_index :unregistered_watchers_notifications, :project_id
-    add_index :unregistered_watchers_notifications, [:issue_status_id, :project_id], :unique => true, name: "index_unreg_watchers_on_issue_status_id_and_project_id"
+    add_index :unregistered_watchers_notifications, :project_id unless index_exists?(:unregistered_watchers_notifications, :project_id)
+    add_index :unregistered_watchers_notifications, [:issue_status_id, :project_id], :unique => true, name: "index_unreg_watchers_on_issue_status_id_and_project_id" unless index_exists?(:unregistered_watchers_notifications, [:issue_status_id, :project_id], :unique => true, name: "index_unreg_watchers_on_issue_status_id_and_project_id")
   end
 
   def self.down
