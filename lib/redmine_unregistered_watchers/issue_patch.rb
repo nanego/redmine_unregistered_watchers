1f19d7a8b8017a3b988066f7db9c1b333966478f
Vincent Robert <vincent.robert@nanego.com>
2016-05-03 11:24:58 +0200

    Revert last tmp commit and fix bug

--- lib/redmine_unregistered_watchers/issue_patch.rb
+++ lib/redmine_unregistered_watchers/issue_patch.rb
@@ -6,7 +6,7 @@ class Issue
   after_create :send_notification_to_unregistered_watchers
 
   def send_notification_to_unregistered_watchers
-    if self.project.module_enabled?("unregistered_watchers") && self.unregistered_watchers.present?
+    if self.project.module_enabled?("unregistered_watchers")
       new_issue_notif = self.project.unregistered_watchers_notifications.where("issue_status_id = ?", IssueStatus.order("position").pluck(:id).first).first
       if new_issue_notif.present?
         Mailer.deliver_issue_to_unregistered_watchers(self, new_issue_notif)
