b5c2f0cc2760f46921f41a534ae8481e2709d071
Vincent Robert <vincent.robert@nanego.com>
2016-06-27 16:51:56 +0200

    Format datetime variables to userfriendly dates

--- app/helpers/unregistered_watchers_helper.rb
+++ app/helpers/unregistered_watchers_helper.rb
@@ -5,7 +5,11 @@ module UnregisteredWatchersHelper
         match = match.gsub('<<', '').gsub('>>', '').to_sym
         value = @issue.try(match)
         if value.present?
-          value.to_s
+          if value.is_a?(Time)
+            value.to_date.to_s
+          else
+            value.to_s
+          end
         else
           ""
         end
