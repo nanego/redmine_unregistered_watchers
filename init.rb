2be6b93877ba5de502d989c4e6e143229825fea2
Vincent Robert <vincent.robert@nanego.com>
2016-07-07 17:08:20 +0200

    Remove 'send last note' feature as it's now easy to include with <<last_note>> variable

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :redmine_unregistered_watchers do
   author 'Vincent ROBERT'
   author_url 'mailto:contact@vincent-robert.com'
   description 'This plugin for Redmine make it possible to notify unregistered users by email when particular events occur on issues.'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/nanego/redmine_unregistered_watchers'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
@@ -28,8 +28,7 @@ Redmine::Plugin.register :redmine_unregistered_watchers do
   settings :default => { 'emails_signature_for_unregistered_watchers' => "",
                          'emails_footer_for_unregistered_watchers' => "",
                          'unregistered_watchers_label' => "Notifier d'autres personnes",
-                         'status' => [],
-                         'send_last_note' => 'false'
+                         'status' => []
                        },
            :partial => 'settings/redmine_plugin_unregistered_watchers'
 end
--- lib/redmine_unregistered_watchers/journal_patch.rb
+++ lib/redmine_unregistered_watchers/journal_patch.rb
@@ -4,24 +4,12 @@ class Journal < ActiveRecord::Base
 
   after_create :send_notification_to_unregistered_watchers
 
-  def current_or_last_note
-    if self.notes.present?
-      self.notes
-    else
-      self.journalized.last_note
-    end
-  end
-
   def send_notification_to_unregistered_watchers
     updated_issue = self.journalized.reload
     if updated_issue.is_a?(Issue) && updated_issue.project.module_enabled?("unregistered_watchers")
       if new_status.present?
         issue_notif = self.issue.project.unregistered_watchers_notifications.find_by_issue_status_id(new_status.id)
         if issue_notif.present?
-          if Setting['plugin_redmine_unregistered_watchers']['send_last_note'] == 'true'
-            note = current_or_last_note
-            issue_notif.email_body = note if note.present?
-          end
           Mailer.deliver_issue_to_unregistered_watchers(updated_issue, issue_notif)
         end
       end
