3778c6c8b8647386f1e845baf7c5b454b7f04909
Vincent Robert <vincent.robert@nanego.com>
2016-10-19 15:46:58 +0200

    Improve compatibility with other plugins

--- app/overrides/issues/_form.rb
+++ app/overrides/issues/_form.rb
@@ -2,8 +2,16 @@ Deface::Override.new :virtual_path  => "issues/_form",
                      :name          => "add-unregistered-watchers-to-issue-form",
                      :insert_after  => "div#attributes",
                      :partial       => "issues/add_unregistered_watchers_to_issue"
+Deface::Override.new :virtual_path  => "issues/_form_with_positions",
+                     :name          => "add-unregistered-watchers-to-issue-form",
+                     :insert_after  => "div#attributes",
+                     :partial       => "issues/add_unregistered_watchers_to_issue"
 
 Deface::Override.new :virtual_path  => "issues/_form",
+                     :name          => "add-force-email-to-unregistered-watchers-checkbox-to-issue-form",
+                     :insert_after  => "div#attributes",
+                     :partial       => "issues/add_force_email_checkbox_to_form"
+Deface::Override.new :virtual_path  => "issues/_form_with_positions",
                      :name          => "add-force-email-to-unregistered-watchers-checkbox-to-issue-form",
                      :insert_after  => "div#attributes",
                      :partial       => "issues/add_force_email_checkbox_to_form"
