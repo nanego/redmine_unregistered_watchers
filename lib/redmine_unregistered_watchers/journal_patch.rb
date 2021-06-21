e3889d4ca1bf47b3a09eb5eeadfbaba05c96c8da
Vincent Robert <vincent.robert@nanego.com>
2021-06-21 15:42:42 +0200

    Fix conflict with AdminActivity plugin when journalized is a project

--- lib/redmine_unregistered_watchers/journal_patch.rb
+++ lib/redmine_unregistered_watchers/journal_patch.rb
@@ -7,8 +7,8 @@ class Journal < ActiveRecord::Base
   after_create_commit :send_notification_to_unregistered_watchers
 
   def send_notification_to_unregistered_watchers
-    updated_issue = self.journalized.reload
-    if updated_issue.is_a?(Issue) && updated_issue.project.module_enabled?("unregistered_watchers")
+    if self.journalized.is_a?(Issue) && self.journalized.project.module_enabled?("unregistered_watchers")
+      updated_issue = self.journalized.reload
       issue_notif = updated_issue.project.unregistered_watchers_notifications.find_by_issue_status_id(updated_issue.status_id)
       if updated_issue.notify_unreg_watchers?(issue_notif)
         Mailer.deliver_issue_to_unregistered_watchers(updated_issue, issue_notif)
