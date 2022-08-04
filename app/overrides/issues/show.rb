1443987e55aa3fdb45b76ea1978cad3109e39d01
Yazan <y.alaeddin@adacis.net>
2022-08-04 11:43:41 +0200

    Ajouet un indice pour les notifications mails

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -18,6 +18,7 @@ Deface::Override.new :virtual_path  => 'issues/show',
                              :created_on => mail.created_at)
   end
   @journals.sort_by!(&:created_on)
+  @journals.each_with_index {|j, i| j.indice = i + 1}
   @journals.reverse! if User.current.wants_comments_in_reverse_order?
 %>
 EOS
