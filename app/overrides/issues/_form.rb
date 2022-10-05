c7d98823d109df5d09e7fe42cf85c2fcfa5a6b6f
Vincent Robert <vincent.robert@nanego.com>
2022-10-05 15:23:53 +0200

    Simplify Deface overrides

--- app/overrides/issues/_form.rb
+++ app/overrides/issues/_form.rb
@@ -1,17 +1,17 @@
-Deface::Override.new :virtual_path  => "issues/_attributes",
+Deface::Override.new :virtual_path  => "issues/_form",
                      :name          => "add-unregistered-watchers-to-issue-form",
-                     :insert_before => 'erb[silent]:contains("include_calendar_headers_tags")',
+                     :insert_bottom => '#attributes',
                      :partial       => "issues/add_unregistered_watchers_to_issue"
 Deface::Override.new :virtual_path  => "issues/_form_with_positions",
                      :name          => "add-unregistered-watchers-to-issue-form",
-                     :insert_before => 'erb[silent]:contains("include_calendar_headers_tags")',
+                     :insert_bottom => '#attributes',
                      :partial       => "issues/add_unregistered_watchers_to_issue"
 
-Deface::Override.new :virtual_path  => "issues/_attributes",
+Deface::Override.new :virtual_path  => "issues/_form",
                      :name          => "add-force-email-to-unregistered-watchers-checkbox-to-issue-form",
-                     :insert_before => 'erb[silent]:contains("include_calendar_headers_tags")',
+                     :insert_bottom => '#attributes',
                      :partial       => "issues/add_force_email_checkbox_to_form"
 Deface::Override.new :virtual_path  => "issues/_form_with_positions",
                      :name          => "add-force-email-to-unregistered-watchers-checkbox-to-issue-form",
-                     :insert_before => 'erb[silent]:contains("include_calendar_headers_tags")',
+                     :insert_bottom => '#attributes',
                      :partial       => "issues/add_force_email_checkbox_to_form"
