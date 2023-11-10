d182070856c34190d78ad7300ab7c57c259fb993
Vincent Robert <vincent.robert@nanego.com>
2023-11-10 09:57:05 +0100

    Clean up migration file

--- db/migrate/006_add_tracker_to_notifications.rb
+++ db/migrate/006_add_tracker_to_notifications.rb
@@ -1,9 +1,7 @@
 class AddTrackerToNotifications < ActiveRecord::Migration[4.2]
   def change
     add_column :unregistered_watchers_notifications, :tracker_id, :integer
-    if index_exists?(:unregistered_watchers_notifications, [:issue_status_id, :project_id], unique: true, name: "index_unreg_watchers_on_issue_status_id_and_project_id")
-      remove_index :unregistered_watchers_notifications, [:issue_status_id, :project_id], unique: true, name: "index_unreg_watchers_on_issue_status_id_and_project_id"
-    end
+    remove_index :unregistered_watchers_notifications, name: "index_unreg_watchers_on_issue_status_id_and_project_id", if_exists: true
     add_index :unregistered_watchers_notifications, [:issue_status_id, :project_id, :tracker_id], unique: true, name: "index_unreg_watchers_on_status_id_and_project_id"
   end
 end
