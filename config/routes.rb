11f728cf4d7d1c78c9e367332ffa238bb8242b51
Vincent Robert <vincent.robert@nanego.com>
2024-05-29 07:03:14 +0200

    Update CI config: add latest Redmine release

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ RedmineApp::Application.routes.draw do
   put :update_status_notifications_per_project, to: "unregistered_watchers#update_status_notifications_per_project"
   get 'unregistered_watchers/settings', to: "unregistered_watchers#settings"
   resources :unregistered_watchers_histories, only: [:show, :index]
-  post 'issues/:issue_id/:history_id/resend_unregistered_watchers_notification', to: 'issues#resend_unregistered_watchers_notification' , as: :resend_unregistered_watchers_notification
+  post 'issues/:issue_id/:history_id/resend_unregistered_watchers_notification', to: 'issues#resend_unregistered_watchers_notification', as: :resend_unregistered_watchers_notification
 end
